ab2e541d0fac351706f7287c89cbc36df0afcfd3
MikeAngel-dev <65077762+MikeAngel-dev@users.noreply.github.com>
2020-06-27 20:58:37 +0200

    new functions
    added a list of all last remindings and added the command /timer

--- client.lua
+++ client.lua
@@ -1,6 +1,16 @@
-
+local hidden = true;
 
 RegisterNetEvent('reminder:PlayNotifySound')
 AddEventHandler('reminder:PlayNotifySound', function()
 	SendNUIMessage({message = 'play'})
+end)
+
+RegisterNetEvent('reminder:list')
+AddEventHandler('reminder:list', function(msg)
+	hidden = not hidden;
+	SendNUIMessage({
+		message = 'list',
+		reminds = msg,
+		hidden = hidden
+	})
 end)
--- server.lua
+++ server.lua
@@ -1,4 +1,3 @@
-
 local reminds = {};
 
 RegisterCommand('remindme', function(source, args, rawCommand)
@@ -109,5 +108,79 @@ Citizen.CreateThread(function()
 		end
 		
 		Citizen.Wait(10000) --every 10 seconds.
+	end
+end)
+
+RegisterCommand('timer', function(source, args, rawCommand)
+	if args[1] ~= nil and args[1] ~= '' then
+		local minu = args[1]
+		local args = string.gsub(table.concat(args, ' '), '^[^%s+]+', '') --remove the first number standing for the minutes.
+		if args ~= '' then
+			if tonumber(minu) >= 1 then
+				local curmin = tonumber(os.date('%M'))
+				local curhour = tonumber(os.date('%H'))
+				for i = 1, math.floor(tonumber(minu)) do
+					curmin = curmin +1
+					if curmin >= 59 then
+						curhour = curhour +1;
+						curmin = 0;
+					end
+					if curhour >= 24 then
+						curhour = 00;
+					end
+				end
+				
+				if curhour < 10 then
+					s_h_time = 0 ..curhour
+				else
+					s_h_time = curhour
+				end
+				if curmin < 10 then
+					s_m_time = 0 ..curmin
+				else
+					s_m_time = curmin
+				end
+				Wait(500)
+				local time = tostring(s_h_time)..':'..tostring(s_m_time)
+				
+				TriggerClientEvent('chat:addMessage', source, {
+					color={255, 0, 0},
+					args={'System', 'You\'ll be reminded at '..time..'!'}
+				})
+				table.insert(reminds, {trigger=time, string=args, user=tonumber(source)})
+			else
+				TriggerClientEvent('chat:addMessage', source, {
+					color={255, 0, 0},
+					args={'System', 'You\'re time needs to be one or higher!'}
+				})
+			end
+		else
+			TriggerClientEvent('chat:addMessage', source, {
+				color={255, 0, 0},
+				args={'System', 'You\'ve forgot to add the reminder text!'}
+			})
+		end
+	end
+end)
+
+RegisterCommand('reminds', function(source)
+	local id = tonumber(source)
+	local msg = 'close this list with /reminds<br><br>'
+	
+	if #reminds > 0 then
+		for i = 1, #reminds do
+			if reminds[i] and reminds[i].user then
+				if reminds[i].user == id then
+					msg = msg..'<br>'..reminds[i].trigger..'|><br>\t'..reminds[i].string
+				end
+			end
+		end
+		
+		TriggerClientEvent('reminder:list', id, msg)
+	else
+		TriggerClientEvent('chat:addMessage', source, {
+			color={255, 0, 0},
+			args={'System', 'You have no reminds!'}
+		})
 	end
 end)
